95c6a7baf01eb674b3644e0ca18ba18a9cb27c07
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2018-08-24 19:04:37 +0200

    Added role list and role commands

---
+++ src/commands/RoleCommand.js
@@ -0,0 +1,77 @@
+
+const Command = require("./Command");
+const Discord = require('discord.js');
+class RoleCommand extends Command {
+
+    getName() {
+        return "role";
+    }
+    getUsage() {
+        return "role <role>"
+    }
+    getGroup(){
+        return "main";
+    }
+    getRoles(){
+        return ["member"];
+    }
+    getHelp() {
+        return "Přidá nebo odebere zadanou roli."
+    }
+
+    init(client, settings, commands) {
+        this.roles = settings.roles;
+    }
+
+    call(args, channel, user){
+        if(args.length != 1){
+            this.sendHelp(channel);
+            return;
+        }
+
+        let role = args[0];
+        let roles = Object.keys(this.roles);
+
+        if(!roles.includes(role)){
+            this.sendError(channel, "Žádnou roli s tímto jménem jsme nenašli. Seznam všech rolí vypíšete pomocí příkazu rolelist.")
+
+            return;
+        }
+        
+        if(role == "member"){
+            this.sendError(channel, "Nemůžete si odebrat zákládni roli pro členství.")
+
+            return;
+        }
+        
+        let guild = channel.guild;
+
+        guild.fetchMember(user).then(member => {
+            let roleId = this.roles[role];
+            if(member.roles.find(r => r.id == roleId) != undefined){
+                member.removeRole(roleId).catch(console.error);
+
+                const embed = new Discord.RichEmbed()
+                    .setTitle("✅ | Role odebrána")
+                    .setDescription("Role " + role + " byla odebrána od tvého účtu.")
+                    .setColor(0xe67e22);
+
+                channel.send(embed);
+            } else {
+                member.addRole(roleId).catch(console.error);
+
+                const embed = new Discord.RichEmbed()
+                    .setTitle("✅ | Role přiřazena")
+                    .setDescription("Role " + role + " byla přiřazena k tvému účtu.")
+                    .setColor(0xe67e22);
+
+                channel.send(embed);
+            }
+        }).catch(console.error);
+
+        return false;
+    }
+
+}
+
+module.exports = RoleCommand;
---
+++ src/commands/RoleListCommand.js
@@ -0,0 +1,49 @@
+
+const Command = require("./Command");
+const Discord = require('discord.js');
+const fs = require('fs');
+
+class RoleListCommand extends Command {
+
+    getName() {
+        return "rolelist";
+    }
+    getUsage() {
+        return "rolelist"
+    }
+    getGroup(){
+        return "main";
+    }
+    getRoles(){
+        return ["member"];
+    }
+    getHelp() {
+        return "Vypíše seznam rolí."
+    }
+
+    init(client, settings, commands) {
+        this.roles = settings.roles;
+    }
+
+    call(args, channel){
+        let list = "";
+        let roles = channel.guild.roles;
+
+        Object.keys(this.roles).forEach(shortcut => {
+            let roleId = this.roles[shortcut];
+
+            list += "`" + shortcut + "` - " + roles.find(role => role.id == roleId) + "\n";
+        });
+
+        let embed = new Discord.RichEmbed()
+            .setTitle("👥 | Seznam rolí")
+            .setDescription(list)
+            .setColor(0xe67e22);
+
+        channel.send(embed);
+        return false;
+    }
+
+}
+
+module.exports = RoleListCommand;
