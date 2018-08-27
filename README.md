2cf59a298b5ead5566295517f0fba570b5436ff4
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2018-08-27 21:51:10 +0200

    Added supplementation and reference to jsdom

--- README.md
+++ README.md
@@ -75,3 +75,4 @@ All dependencies will be download by node automaticaly.
 | ------ | ------ |
 | Discord.js | 11.3.2 |
 | moment | 2.22.2 |
+| jsdom | 12.0.0 |
---
+++ src/modules/SupplementationModule.js
@@ -0,0 +1,118 @@
+const Module = require("./Module");
+const http = require('http');
+const fs = require('fs');
+const jsdom = require("jsdom");
+const Discord = require('discord.js');
+const { JSDOM } = jsdom;
+
+class SupplementationModule extends Module {
+
+    getName() {
+        return "supplementationmodule";
+    }
+
+    init(client, settings, commands) {
+        this.webOptions = {
+            host: 'ssps.cz',
+            path: '/student/'
+        }
+        this.client = client;
+        this.supplementationHighlight = settings["supplementation-highlight"];
+        this.channel = settings["channels"]["supplementation"];
+        this.tick();
+        setInterval(() => this.tick(), 1800000);
+    }
+
+    tick(){
+        let request = http.request(this.webOptions, (res)=> {
+            let data = '';
+            res.on('data', function (chunk) {
+                data += chunk;
+            });
+            res.on('end', ()=> {
+                const dom = new JSDOM(data, {
+                    url: "http://ssps.cz/student/",
+                    referrer: "http://ssps.cz/student/",
+                    contentType: "text/html",
+                    includeNodeLocations: true,
+                    storageQuota: 10000000
+                });
+        
+                let days = {};
+                let htmlDays = dom.window.document.querySelectorAll(".nav.nav-tabs")[0].children;
+                Object.values(htmlDays).forEach(htmlDay => {
+                    let link = htmlDay.querySelectorAll("a")[0];
+                    days[link.textContent.trim()] = link.getAttribute("href");
+                });
+
+                let supples = {}
+                Object.keys(days).forEach(dayName => {
+                    let htmlObject = dom.window.document.querySelectorAll(days[dayName])[0];
+                    let list = htmlObject.querySelectorAll("ul")[0];
+
+                    supples[dayName] = [];
+                    Object.values(list.children).forEach(item => {
+                        supples[dayName].push(item.textContent.trim());
+                    });
+                });
+
+
+                let supplementations = fs.readFileSync("./temp/supplementations.json", "utf8");
+                let supplementationsObject = JSON.parse(supplementations); 
+                                
+                let channel = this.client.channels.find(c => c.id == this.channel);
+                
+                let count = 0;
+                Object.keys(supples).forEach(dayName => {
+                    count++;
+                    let supplesList = supples[dayName];
+                    let suppleString = "";
+
+                    supplesList.forEach(supple => {
+                        if(supple.includes(this.supplementationHighlight)){
+                            suppleString += "**" + supple + "**\n";
+                            return;
+                        }
+                        suppleString += supple + "\n";
+                    });
+
+                    suppleString = suppleString.replace(/^\s+|\s+$/g, '');
+
+                    const embed = new Discord.RichEmbed()
+                        .setTitle("👓 | Bylo zveřejněno nové suplování na den/dny " + dayName)
+                        .setDescription(suppleString)
+                        .setColor(0x1abc9c);
+
+                    if(supplementationsObject["supplementations"][dayName] != undefined){
+                        let messageId = supplementationsObject["supplementations"][dayName];
+
+                        channel.fetchMessage(messageId).then(message => {
+                            if(message.embeds[0].description !== suppleString){
+                                message.edit(embed);
+                            }
+                        });
+                    } else {
+                        channel.send(embed).then(message => {
+                            supplementationsObject["supplementations"][dayName] = message.id;
+
+                            if(Object.keys(supples).length == count){
+                                fs.writeFileSync("./temp/supplementations.json", JSON.stringify(supplementationsObject));
+                            }
+                        });
+                    }
+                });
+            });
+        });
+        request.on('error', function (e) {
+            console.log(e.message);
+        });
+        request.end();
+    }
+
+    event(name, args){
+
+    }
+
+}
+
+module.exports = SupplementationModule;
