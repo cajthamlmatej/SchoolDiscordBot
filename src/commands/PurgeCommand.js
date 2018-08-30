1c44c44c6d97abe825282f3fa026e9e8f89c65eb
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2018-08-30 13:21:26 +0200

    Purge command now can remove up to 1000 messages.

--- src/commands/PurgeCommand.js
+++ src/commands/PurgeCommand.js
@@ -29,17 +29,34 @@ class PurgeCommand extends Command {
 
         let count = args[0];
 
-        if(count <= 0 || count > 100){
+        if(count <= 0 || count > 1000){
             this.sendError(channel, "Zadaný počet zpráv, které se mají smazat není správný. Minimum zpráv je 1 a nejvíce je 100.")
             return;
         }
         
-        channel.fetchMessages({ limit: count })
-            .then(messages => {
-                channel.bulkDelete(messages);
-            })
-            .catch(console.error);
+        let left = count;
+        let interval = setInterval(() => {
+            if(left < 0){
+                clearInterval(interval);
+                return;
+            }
 
+            let toDelete = 0;
+            if(left > 100){
+                toDelete = 100;
+            } else {
+                toDelete = left;
+            }
+
+            left -= toDelete;
+
+            channel.fetchMessages({ limit: toDelete })
+                .then(messages => {
+                    channel.bulkDelete(messages);
+                })
+                .catch(console.error);
+        }, 100);
+        
         return false;
     }
 
