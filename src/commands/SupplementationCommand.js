f6f742ddd0ede193eaabca47c7c6d20e8c14aa27
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2018-09-14 19:07:36 +0200

    Resolve #52

--- src/commands/SupplementationCommand.js
+++ src/commands/SupplementationCommand.js
@@ -1,5 +1,7 @@
 const SubsCommand = require("./SubsCommand");
 const Discord = require('discord.js');
+const moment = require('moment');
+const Translation = require("../Translation");
 
 class SupplementationCommand extends SubsCommand {
 
@@ -8,7 +10,11 @@ class SupplementationCommand extends SubsCommand {
             "refresh": {
                 "arguments": 0,
                 "roles": ["moderator"]
-            }
+            },
+            "lastcheck": {
+                "arguments": 0,
+                "roles": ["member"]
+            },
         }
     }
 
@@ -35,6 +41,19 @@ class SupplementationCommand extends SubsCommand {
     callRefresh(args, message) {
         let channel = message.channel;
         this.supplementationModule.refresh(channel);
+
+        return false;
+    }
+
+    callLastcheck(args, message){
+        let channel = message.channel;
+
+        const embed = new Discord.RichEmbed()
+            .setTitle("👓 | " + Translation.translate("command.supplementation.lastcheck.title"))
+            .setColor(0xbadc58)
+            .setDescription(Translation.translate("command.supplementation.lastcheck") + " " + moment().diff(this.supplementationModule.lastCheck, "minutes") + "m");
+
+        channel.send(embed);
 
         return false;
     }
