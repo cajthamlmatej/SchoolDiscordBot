8e77b03838f31961ef6c875614ae623704040c06
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2018-09-21 21:54:51 +0200

    Progress #64

--- src/CommandBuilder.js
+++ src/CommandBuilder.js
@@ -1,19 +1,87 @@
 const Discord = require('discord.js');
+const Translation = require("./Translation");
 
 class CommandBuilder {
 
-    constructor(user, fields, channel){
-        this.build = {user: user, fields: fields, channel: channel};
+    constructor(user, fields, channel, title){
+        this.build = {user: user, fields: fields, title: title, channel: channel};
         this.field = 0;
+        this.stopWord = "stop";
+        this.values = {};
     }
 
     start() {
-        this.collector = new Discord.MessageCollector(this.build.channel, m => m.author.id === this.build.user.id, { time: 10000 });
-        this.collector.on("collect", this.collect);
+        this.collector = new Discord.MessageCollector(this.build.channel, m => m.author.id === this.build.user.id);
+        this.collector.on("collect", (message) => { this.collect(message); });
+        this.collector.on("end", (messages, reason) => { this.end(messages, reason); });
+
+        this.build.channel.send(this.generateEmbed(this.build.fields[this.field])).then(message => {
+            this.message = message;
+        });
     }
 
     collect(message) {
-        console.log("collected: " + message.content);
+        let messageContent = message.content;
+        console.log("collected: " + messageContent);
+    
+        if(messageContent.toLowerCase() == this.stopWord.toLowerCase()){
+            this.collector.stop("forced");
+        } else {
+            let field = this.build.fields[this.field]
+            let passed = field.validate(messageContent);
+
+            if(passed === true){
+                if(field.value != undefined){    
+                   this.values[field.name] = field.value(messageContent, this.values);
+                } else {
+                    this.values[field.name] = messageContent;
+                }
+
+                if(this.build.fields[this.field + 1] == undefined){
+                    this.collector.stop("fieldEnd");
+                } else {
+                    this.field += 1;
+
+                    this.message.edit(this.generateEmbed(this.build.fields[this.field], passed));
+                }
+            }
+            message.delete();
+        }
+    }
+
+    end(messages, reason){
+        console.log("Collector end, reason: " + reason);
+    }
+
+    generateEmbed(field, error){
+        let description = "";
+
+        description += field.title + "\n\n";
+        description += field.help + "\n";
+        if(Array.isArray(field.example)){
+            description += "Example: ";
+            let list = [];
+
+            field.example.forEach(example => {
+                list.push("`" + example + "`");
+            });
+
+            description += list.join(" / ");
+        } else 
+            description += "Example: `" + field.example + "`\n";
+        
+
+        let embed = new Discord.RichEmbed()
+            .setTitle(Translation.translate(this.build.title))
+            .setDescription(description)
+            .setColor(0x34ace0)
+            .setFooter(this.build.user.username, this.build.user.avatarURL);
+
+        if(error != undefined && error != true){
+            embed.addField("Chyba", error);
+        }
+
+        return embed;
     }
 
 }
--- src/commands/TestCommand.js
+++ src/commands/TestCommand.js
@@ -1,6 +1,7 @@
 const Command = require("./Command");
 const Discord = require('discord.js');
 const CommandBuilder = require("../CommandBuilder");
+const moment = require('moment');
 
 class TestCommand extends Command {
 
@@ -13,28 +14,109 @@ class TestCommand extends Command {
     }
 
     init(bot) {
-        this.client = bot.client;
+        this.eventModule = bot.modules["eventmodule"];
     }
 
     call(args, message) {
         let channel = message.channel;
         
+        let types = ["event", "task"];
         let builder = new CommandBuilder(message.author, [
             {
-                "title": "",
-                "help": "",
+                "name": "name",
+                "title": "Jméno události / úkolu",
+                "help": "Zadejte unikátní jméno eventu pro jeho budoucí manipulaci.",
+                "example": "stp_ukol_potreby",
                 "validate": (content) => {
-                    return false;
+                    if(this.eventModule.exists(content)){
+                        return "command.event.already-exists";
+                    } else {
+                        return true;
+                    }
                 }
             },
             {
-                "title": "",
-                "help": "",
+                "name": "type",
+                "title": "Typ eventu",
+                "help": "Zadejte, o jaký typ eventu se jedná.",
+                "example": types,
+                "validate": (content) => {
+                    if(!types.includes(content)){
+                        return "command.event.type-not-valid";
+                    } else 
+                        return true;
+                }
+            },
+            {
+                "name": "start",
+                "title": "Začátek eventu",
+                "help": "Zadejte datum (a čas) začátku události.",
+                "example": ["11. 9. 2018", "11. 9. 2018 8:00"],
+                "validate": (content) => {
+                    if(!(moment(content, "D. M. YYYY").isValid() || moment(content, "D. M. YYYY").isValid())){
+                        return "command.event.wrong-date-format";
+                    } else
+                        return true;
+                }
+            },
+            {
+                "name": "end",
+                "title": "Konec eventu",
+                "help": "Zadejte datum (a čas) konce události nebo nevyplnujte (použijte znak \"-\").",
+                "example": ["-", "12. 9. 2018", "12. 9. 2018 13:30"],
+                "validate": (content) => {
+                    if(content == "-")
+                        return true;
+
+                    if(!(moment(content, "D. M. YYYY").isValid() || moment(content, "D. M. YYYY").isValid())){
+                        return "command.event.wrong-date-format";
+                    } else
+                        return true;
+                },
+                "value": (content, values) => {
+                    return values["start"];
+                }
+            },
+            {
+                "name": "role",
+                "title": "Role",
+                "help": "Zadejte jméno role, pro kterou je event určený.",
+                "example": "member",
+                "validate": (content) => {
+                    if(!this.eventModule.isMentionableRole(content)){
+                        return "command.event.role-not-valid";
+                    } else
+                        return true;
+                }
+            },
+            {
+                "name": "place",
+                "title": "Místo",
+                "help": "Zadejte místo, kde se event udehrává.",
+                "example": ["Škola", "Arbesovo náměstí"],
+                "validate": (content) => {
+                    return true;
+                }
+            },
+            {
+                "name": "subject",
+                "title": "Předmět",
+                "help": "Zadejte předmět o kterem event je.",
+                "example": ["STP", "?"],
+                "validate": (content) => {
+                    return true;
+                }
+            },
+            {
+                "name": "description",
+                "title": "Popis",
+                "help": "Zadejte jakékoliv další informace, které o eventu víte.",
+                "example": "Vytvořte svůj vlastní životopis a odevzdejte ho do uvedeného datumu. Platí i pro lidi, které se hodiny nezúčastnili.",
                 "validate": (content) => {
                     return true;
                 }
             }
-        ], channel);
+        ], channel, "Přidání nové události / úkolu");
         
         builder.start();
 
