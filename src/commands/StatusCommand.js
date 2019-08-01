50b06a17bfe8ee03e3cc73ad170239bf133636b8
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2019-08-01 18:03:24 +0200

    Resolve #144

--- src/commands/StatusCommand.js
+++ src/commands/StatusCommand.js
@@ -2,6 +2,7 @@ const Command = require("./Command");
 const Discord = require("discord.js");
 const Translation = require("../Translation");
 const moment = require("moment");
+const SubsCommand = require("./SubsCommand");
 
 class StatusCommand extends Command {
 
@@ -33,11 +34,23 @@ class StatusCommand extends Command {
         const duration = moment.duration(moment().diff(this.startTime));
         const hours = parseInt(duration.asHours());
         const minutes = parseInt(duration.asMinutes()) % 60;
+
+        let numberOfSubCommands = 0;
+        let numberOfSubCommandsDisabled = 0;
+
+        Object.values(this.commands).forEach(command => {
+            if(command instanceof SubsCommand)
+                numberOfSubCommands += Object.keys(command.getSubCommands()).length;
+        });
+        Object.values(this.disabledCommands).forEach(command => {
+            if(command instanceof SubsCommand)
+                numberOfSubCommandsDisabled += Object.keys(command.getSubCommands()).length;
+        });
         
         const embed = new Discord.RichEmbed()
             .setTitle("📊 | " + Translation.translate("command.status.stats"))
             .setColor(0xbadc58)
-            .addField(Translation.translate("command.status.commands"), Translation.translate("command.status.number-of-commands", Object.keys(this.commands).length, Object.keys(this.disabledCommands).length), true)
+            .addField(Translation.translate("command.status.commands"), Translation.translate("command.status.number-of-commands", Object.keys(this.commands).length + " (+ " + numberOfSubCommands + " sub)", Object.keys(this.disabledCommands).length + " (+ " + numberOfSubCommandsDisabled + " sub)"), true)
             .addField(Translation.translate("command.status.modules"), Translation.translate("command.status.number-of-modules", Object.keys(this.modules).length, Object.keys(this.disabledModules).length), true)
             .addField(Translation.translate("command.status.ping"), Math.round(this.client.ping) + "ms", true)
             .addField(Translation.translate("command.status.author"), "Matěj Cajthaml [source (GitHub)](https://github.com/cajthamlmatej/SchoolDiscordBot)", true)
