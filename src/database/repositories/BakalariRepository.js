b21b19130f4250a47a837424e334da25187e2b70
Matěj Cajthaml <cajthaml.matej@navarru.cz>
2019-08-04 14:40:30 +0200

    Removed unused stuff

--- src/database/repositories/BakalariRepository.js
+++ src/database/repositories/BakalariRepository.js
@@ -15,23 +15,6 @@ class BakalariRepository extends Repository {
         return entity != null;
     }
 
-    async getAnnoucementByName(name, fields = null){
-        return await this.entity.findOne({ name: name, deleted: false }, fields);
-    }
-
-    async deleteAnnoucement(name){
-        await this.entity.updateOne({ name: name, deleted: false }, {
-            deleted: true
-        });
-    }
-    async editAnnoucement(name, values) {
-        await this.entity.updateOne({ name: name, deleted: false }, values);
-    }
-
-    async getAnnoucements(fields = null) {
-        return await this.entity.find({ deleted: false }, fields);
-    }
-
 }
 
 module.exports = BakalariRepository;
